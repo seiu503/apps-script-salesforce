3b98cbfbd6b839847dc14d44d7ea1603ce82cc20
sarah <rifkegribenes@gmail.com>
2025-06-23 13:17:17 -0700

    generalized getUserByEmail to work w dev or prod

--- GetUserByEmail.js
+++ GetUserByEmail.js
@@ -1,17 +1,20 @@
 const userIds = users.getRange("A2:A").getValues().flat().filter(Boolean);
 
-async function getUserByEmail(email = 'rooneyn@seiu503.org') {
-  console.log(`getUserByEmail.gs > 2, email: ${email}`);
+async function getUserByEmail(email, env) {
+  const url = env === 'prod' ? SW_PROD_SHEET_URL : SW_DEV_SHEET_URL;
+  const userSheet = SpreadsheetApp.openByUrl(url).getSheetByName('Users');
+  console.log(`getUserByEmail.gs > 2, email: ${email}, env: ${env}`);
   if (email) {
     const qp = new QueryParameters();
     qp.setSelect("Id, ContactId, Salutation_Last__c, Salutation_Name__c, Email, AppSheetTurf__c");
     qp.setFrom("User");
-    qp.setWhere(`Email = \'${email}\' AND profileID__c = '00eRf000009g4rd'`); // customer community login SEIU 503 -- may need to change for production
+    // qp.setWhere(`Email = \'${email}\' AND profileID__c = '00eRf000009g4rd'`); // customer community login SEIU 503 -- may need to change for production
+    qp.setWhere(`Email = \'${email}\'`); 
     try {
-      const records = get(qp);
+      const records = get(qp, '50', env);
       console.log('getUser payload:');
       console.log(records);
-      await setUser(records);
+      await setUser(records, userSheet);
       return{
         Turf: records[0].AppSheetTurf__c,
         Success: true,
@@ -51,11 +54,11 @@ async function appendNewRow(obj, sheet) { // expects a single object
     }
 }
 
-async function setUser(payload) {
+async function setUser(payload, sheet) {
   console.log('setUser > 33');
 
   // Check for matching rows -- has this user been pulled before?
-  const allData = users.getDataRange().getValues();
+  const allData = sheet.getDataRange().getValues();
   let userRowIndex;
   const user = allData.filter((row,index) => {
     // console.log('setUser > 39');
@@ -77,7 +80,7 @@ async function setUser(payload) {
     // append new row with data from paylod from getUserByEmail function
     console.log(`setUser > 44: no matching user found, appending new data`);
     try {
-      await appendNewRow(payload, users);    
+      await appendNewRow(payload, sheet);    
     } catch (err) {
       console.log(err);
       logErrorFunctions('setUser', payload, userRowIndex, err);
@@ -87,9 +90,9 @@ async function setUser(payload) {
     console.log(`setUser > 34: found matching user, deleting and replacing with this payload`);
     console.log(payload);
     try {
-      users.deleteRow(userRowIndex);
+      sheet.deleteRow(userRowIndex);
       // append new rows with data from paylod from getUser function
-      await appendNewRow(payload, users); 
+      await appendNewRow(payload, sheet); 
     } catch (err) {
       console.log(err);
       logErrorFunctions('setUser', payload, userRowIndex, err);
