85dc214b23508df51c24001e2497789139f34f27
sarah <24995773+rifkegribenes@users.noreply.github.com>
2025-11-21 11:04:54 -0800

    add config for cascade campaign

--- global/globalConfig.js
+++ global/globalConfig.js
@@ -56,6 +56,34 @@ const globalConfig = (campaign, turf) => {
         'Sticker_Up_Participation__c',
         'Bargaining_Team__c'
       ],
+      cascade: [
+        'Id', 
+        'First_name_from_form__c', 
+        'Preferred_Name_from_Form__c',
+        'Last_Name_from_form__c',
+        'Pronouns__c',
+        'Email_from_form__c', 
+        'AGENCY_from_form__c', 
+        'Job_Title__c', 
+        'Preferred_Language_from_form__c', 
+        'Phone_from_form__c', 
+        'Address__c',
+        'City__c',
+        'State__c',
+        'ZIP__c',
+        'Potential_Leader__c',
+        'In_Unit__c',
+        'AppSheet_ID__c',
+        'Shift__c',
+        'Signed_Auth_Card__c',
+        'Auth_Card_Date__c',
+        'Signed_Membership_Form__c',	
+        'Member_Form_Signed_Date_Time__c',
+        'Bargaining_Survey_Complete__c',
+        'Bargaining_Survey_Completed_DATE__c',
+        'Sticker_Up_Participation__c',
+        'Bargaining_Team__c'
+      ],
       jacksonCounty: [
         'Id',
         'First_name_from_form__c',
@@ -103,30 +131,35 @@ const globalConfig = (campaign, turf) => {
       studentWorkers: 'Higher_Ed_Strike_Pledge__c',
       sapphireCC: 'Higher_Ed_Strike_Pledge__c',
       jacksonCounty: 'Higher_Ed_Strike_Pledge__c',
+      cascade: 'Higher_Ed_Strike_Pledge__c',
       memberLeader: 'Contact'
     },
     qpWhere: {
       studentWorkers: `Campaign_Name_Picklist__c = 'Student Workers'`,
       sapphireCC: `Campaign_Name_Picklist__c = 'Sapphire'`,
       jacksonCounty: `Campaign_Name_Picklist__c = 'Jackson County Libraries'`,
+      cascade: `Campaign_Name_Picklist__c = 'Cascade Terrace PA 2025'`,
       memberLeader: `Employer_Name_Text__c = \'${turf}\' AND Active_Worker__c = TRUE`
     },
     campaignPicklist: {
       studentWorkers: 'Sapphire',
       sapphireCC: 'Student Workers',
       jacksonCounty: 'Jackson County Libraries',
+      cascade: 'Cascade Terrace PA 2025'
     },
     sheetURL: {
       studentWorkers: 'https://docs.google.com/spreadsheets/d/14a5ZRXFbAl69VQ98aJ1lCnWLcfh3mhZrKpsxAT01btA/edit',
       sapphireCC: 'https://docs.google.com/spreadsheets/d/1JktJuDesKyZMW6gcM2wRNzIs-BAfobNcC6DuvYg_itM/edit',
       jacksonCounty: 'https://docs.google.com/spreadsheets/d/1M5ceAgQIXuM19Nq-WHBJAoZu8dkOWJevto8dbL9Gglk/edit',
-      memberLeader: 'https://docs.google.com/spreadsheets/d/11pYOEoAtTtxH_5y6uxzVuhUis6DvZxFJoazHnuMD4R4/edit'
+      memberLeader: 'https://docs.google.com/spreadsheets/d/11pYOEoAtTtxH_5y6uxzVuhUis6DvZxFJoazHnuMD4R4/edit',
+      cascade: 'https://docs.google.com/spreadsheets/d/1IOpNNpnpHpn8sHs2ejjsVtcrR_UuKHn7T5Qs8hyq6_0/edit'
     },
     workerSheetName: {
       studentWorkers: 'StudentWorkers',
       sapphireCC: 'Workers',
       jacksonCounty: 'Workers',
-      memberLeader: 'MemberChartingApp'
+      memberLeader: 'MemberChartingApp',
+      cascade: 'Workers'
     },
     // CAIDColumn: {
     //   studentWorkers: 'A2:A',
@@ -138,22 +171,26 @@ const globalConfig = (campaign, turf) => {
       studentWorkers: '',
       sapphireCC: 'Q2:Q',
       jacksonCounty: 'Q2:Q',
-      memberLeader: ''
+      memberLeader: '',
+      cascade: 'Q2:Q'
     },  
     lastSFColumn: {
       studentWorkers: 'AB',
       sapphireCC: 'W',
-      jacksonCounty: 'AA'
+      jacksonCounty: 'AA',
+      cascade: 'Y'
     },
     sheetShortVar: {
       studentWorkers: 'swss',
       sapphireCC: 'spss',
-      jacksonCounty: 'jcss'
+      jacksonCounty: 'jcss',
+      cascade: 'ctss'
     },
     env: {
       studentWorkers: 'prod',
       sapphireCC: 'prod',
-      jacksonCounty: 'prod'
+      jacksonCounty: 'prod',
+      cascade: 'prod'
     }
   }
 
--- global/global_GetUserByEmail.js
+++ global/global_GetUserByEmail.js
@@ -1,6 +1,6 @@
 
 
-async function getUserByEmail(email='kressk@seiu503.org', env = 'prod', campaign='sapphireCC') {
+async function getUserByEmail(email='downeyj@seiu503.org', env = 'prod', campaign='sapphireCC') {
 
   const config = globalConfig(campaign);
   const ss = SpreadsheetApp.openByUrl(config.sheetURL);
